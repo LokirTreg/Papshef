6363611d53d768a9a268fed3aeb1a7988f9f29a7
LokirTreg <alex.popovich2003@mail.ru>
2024-11-14 03:55:35 +0400

    3

--- Papchef/Program.cs
+++ Papchef/Program.cs
@@ -1,159 +1,161 @@
-﻿public class TerminalSymbol
+﻿public class SymbolTable
 {
-    public int Index { get; set; }
-    public string Symbol { get; set; }
-    public string Category { get; set; }
-    public string Type { get; set; }
-    public string Comment { get; set; }
-}
-
-public class TerminalSymbolTable
-{
-    public static List<TerminalSymbol> GetTable()
+    public static List<Tuple<string, string, ТипЛексемы, string>> ТаблицаЗаданныхСимволов()
     {
-        return new List<TerminalSymbol>
+        return new List<Tuple<string, string, ТипЛексемы, string>>
         {
-            new TerminalSymbol { Index = 0, Symbol = "do", Category = "Ключевое слово", Type = "КлючевоеСлово_Do", Comment = "Стартовое слово" },
-            new TerminalSymbol { Index = 1, Symbol = "while", Category = "Ключевое слово", Type = "КлючевоеСлово_While", Comment = "Начало заголовка цикла" },
-            new TerminalSymbol { Index = 2, Symbol = "loop", Category = "Ключевое слово", Type = "КлючевоеСлово_End", Comment = "Конец тела цикла" },
-            new TerminalSymbol { Index = 3, Symbol = "and", Category = "Ключевое слово", Type = "КлючевоеСлово_And", Comment = "Логическая операция 'И'" },
-            new TerminalSymbol { Index = 4, Symbol = "or", Category = "Ключевое слово", Type = "КлючевоеСлово_Or", Comment = "Логическая операция 'ИЛИ'" },
-            new TerminalSymbol { Index = 5, Symbol = ";", Category = "Ключевое слово", Type = "КонецПредусловияЦикла", Comment = "Конец предусловия цикла" },
-            new TerminalSymbol { Index = 6, Symbol = "<", Category = "Специальный символ", Type = "ОператорСравнения", Comment = "Операция сравнения 'меньше'" },
-            new TerminalSymbol { Index = 7, Symbol = "<=", Category = "Специальный символ", Type = "ОператорСравнения", Comment = "Операция сравнения 'меньше или равно'" },
-            new TerminalSymbol { Index = 8, Symbol = "!=", Category = "Специальный символ", Type = "ОператорСравнения", Comment = "Операция сравнения 'неравно'" },
-            new TerminalSymbol { Index = 9, Symbol = "==", Category = "Специальный символ", Type = "ОператорСравнения", Comment = "Операция сравнения 'равно'" },
-            new TerminalSymbol { Index = 10, Symbol = "=", Category = "Специальный символ", Type = "ОператорПрисваивания", Comment = "Операция присваивания" },
-            new TerminalSymbol { Index = 11, Symbol = "+", Category = "Специальный символ", Type = "АрифметическийОператор", Comment = "Операция сложения" },
-            new TerminalSymbol { Index = 12, Symbol = "-", Category = "Специальный символ", Type = "АрифметическийОператор", Comment = "Операция вычитания" },
-            new TerminalSymbol { Index = 13, Symbol = "*", Category = "Специальный символ", Type = "АрифметическийОператор", Comment = "Операция умножения" },
-            new TerminalSymbol { Index = 14, Symbol = "/", Category = "Специальный символ", Type = "АрифметическийОператор", Comment = "Операция деления" },
-            new TerminalSymbol { Index = 15, Symbol = ">", Category = "Специальный символ", Type = "ОператорСравнения", Comment = "Операция сравнения 'больше'" },
-            new TerminalSymbol { Index = 16, Symbol = ">=", Category = "Специальный символ", Type = "ОператорСравнения", Comment = "Операция сравнения 'больше или равно'" }
+            new Tuple<string, string, ТипЛексемы, string>("do", "Ключевое слово", ТипЛексемы.КлючевоеСлово_Do, "Стартовое слово"),
+            new Tuple<string, string, ТипЛексемы, string>("while", "Ключевое слово", ТипЛексемы.КлючевоеСлово_While, "Начало заголовка цикла"),
+            new Tuple<string, string, ТипЛексемы, string>("loop", "Ключевое слово", ТипЛексемы.КлючевоеСлово_End, "Конец тела цикла"),
+            new Tuple<string, string, ТипЛексемы, string>("and", "Ключевое слово", ТипЛексемы.КлючевоеСлово_And, "Логическая операция 'И'"),
+            new Tuple<string, string, ТипЛексемы, string>("or", "Ключевое слово", ТипЛексемы.КлючевоеСлово_Or, "Логическая операция 'ИЛИ'"),
+            new Tuple<string, string, ТипЛексемы, string>(";", "Ключевое слово", ТипЛексемы.КонецПредусловияЦикла, "Конец предусловия цикла"),
+            new Tuple<string, string, ТипЛексемы, string>("<", "Специальный символ", ТипЛексемы.ОператорСравнения, "Операция сравнения 'меньше'"),
+            new Tuple<string, string, ТипЛексемы, string>("<=", "Специальный символ", ТипЛексемы.ОператорСравнения, "Операция сравнения 'меньше или равно'"),
+            new Tuple<string, string, ТипЛексемы, string>("!=", "Специальный символ", ТипЛексемы.ОператорСравнения, "Операция сравнения 'неравно'"),
+            new Tuple<string, string, ТипЛексемы, string>("==", "Специальный символ", ТипЛексемы.ОператорСравнения, "Операция сравнения 'равно'"),
+            new Tuple<string, string, ТипЛексемы, string>("=", "Специальный символ", ТипЛексемы.ОператорПрисваивания, "Операция присваивания"),
+            new Tuple<string, string, ТипЛексемы, string>("+", "Специальный символ", ТипЛексемы.АрифметическийОператор, "Операция сложения"),
+            new Tuple<string, string, ТипЛексемы, string>("-", "Специальный символ", ТипЛексемы.АрифметическийОператор, "Операция вычитания"),
+            new Tuple<string, string, ТипЛексемы, string>("*", "Специальный символ", ТипЛексемы.АрифметическийОператор, "Операция умножения"),
+            new Tuple<string, string, ТипЛексемы, string>("/", "Специальный символ", ТипЛексемы.АрифметическийОператор, "Операция деления"),
+            new Tuple<string, string, ТипЛексемы, string>(">", "Специальный символ", ТипЛексемы.ОператорСравнения, "Операция сравнения 'больше'"),
+            new Tuple<string, string, ТипЛексемы, string>(">=", "Специальный символ", ТипЛексемы.ОператорСравнения, "Операция сравнения 'больше или равно'")
         };
     }
-
-    public static void PrintTable()
+    public static void ВывестиТаблицу()
     {
-        var table = GetTable();
-        Console.WriteLine("{0,-5} {1,-10} {2,-20} {3,-25} {4,-40}", "Индекс", "Символ", "Категория", "Тип", "Комментарий");
-        foreach (var symbol in table)
+        Console.WriteLine("{0,-7} | {1,-20} | {2,-25} | {3,-40} |", "Символ", "Категория", "Тип", "Комментарий");
+        foreach (var символ in ТаблицаЗаданныхСимволов())
         {
-            Console.WriteLine("{0,-5} {1,-10} {2,-20} {3,-25} {4,-40}",
-                              symbol.Index, symbol.Symbol, symbol.Category, symbol.Type, symbol.Comment);
+            Console.WriteLine("{0,-7} | {1,-20} | {2,-25} | {3,-40} |",
+                              символ.Item1, символ.Item2, символ.Item3, символ.Item4);
         }
     }
 }
-public class Lexem
+public class Лексема
 {
     public string Value { get; }
-    public LexemType Type { get; }
+    public ТипЛексемы Type { get; }
     public int Position { get; }
 
-    public Lexem(string value, LexemType type, int position)
+    public Лексема(string value, ТипЛексемы type, int position)
     {
         Value = value;
         Type = type;
         Position = position;
     }
 }
-public class Lexer
+public class ЛексическийАнализ
 {
-    private readonly string _input;
-    private int _position;
+    private readonly string _входнаяСтрока;
+    private int _позиция;
 
-    public Lexer(string input)
+    public ЛексическийАнализ(string input)
     {
-        _input = input;
-        _position = 0;
+        _входнаяСтрока = input;
+        _позиция = 0;
     }
 
-    public List<Lexem> Analyze()
+    public List<Лексема> Анализ()
     {
-        var tokens = new List<Lexem>();
-        while (_position < _input.Length)
+        var лексемы = new List<Лексема>();
+        while (_позиция < _входнаяСтрока.Length)
         {
-            if (char.IsWhiteSpace(CurrentChar()))
+            if (char.IsWhiteSpace(ТекущийСимвол()))
             {
-                _position++;
+                _позиция++;
             }
-            else if (IsLetter(CurrentChar()))
+            else if (IsLetter(ТекущийСимвол()))
             {
                 var Идентификатор = ReadWhile(IsLetterOrDigit);
-                tokens.Add(new Lexem(Идентификатор, IdentifyKeyword(Идентификатор), _position));
+                лексемы.Add(new Лексема(Идентификатор, IdentifyKeyword(Идентификатор), _позиция));
             }
-            else if (char.IsDigit(CurrentChar()))
+            else if (char.IsDigit(ТекущийСимвол()))
             {
                 var Константа = ReadWhile(char.IsDigit);
-                tokens.Add(new Lexem(Константа, LexemType.Константа, _position));
+                лексемы.Add(new Лексема(Константа, ТипЛексемы.Константа, _позиция));
             }
-            else if (CurrentChar() == ';')
+            else if (ТекущийСимвол() == ';')
             {
-                var symbol = ReadWhile(c => c == ';');
-                tokens.Add(new Lexem(symbol, LexemType.КонецПредусловияЦикла, _position));
+                var символ = ReadWhile(c => c == ';');
+                лексемы.Add(new Лексема(символ, ТипЛексемы.КонецПредусловияЦикла, _позиция));
             }
-            else if (CurrentChar() == '<' || CurrentChar() == '='|| CurrentChar() == '>' || CurrentChar() == '!')
+            else if (ТекущийСимвол() == '<' || ТекущийСимвол() == '=' || ТекущийСимвол() == '>' || ТекущийСимвол() == '!')
             {
-                var h = CurrentChar();
-                var symbol = ReadWhile(c => c == '<' || c == '=' || c == '!'|| c == '>');
-                if (symbol == "=")
+                var символ = ReadWhile(c => c == '<' || c == '=' || c == '!' || c == '>');
+
+                var операторыСравнения = SymbolTable.ТаблицаЗаданныхСимволов().GroupBy(i => i.Item3).FirstOrDefault(i => i.Key == ТипЛексемы.ОператорСравнения).Select(i => i.Item1).ToList();
+                if (символ == "=")
                 {
-                    tokens.Add(new Lexem(symbol, LexemType.ОператорПрисваивания, _position));
+                    лексемы.Add(new Лексема(символ, ТипЛексемы.ОператорПрисваивания, _позиция));
+                }
+                else if (операторыСравнения.Contains(символ))
+                {
+                    лексемы.Add(new Лексема(символ, ТипЛексемы.ОператорСравнения, _позиция));
                 }
                 else
                 {
-                    tokens.Add(new Lexem(symbol, LexemType.ОператорСравнения, _position));
+                    лексемы.Add(new Лексема(символ, ТипЛексемы.Неизвестно, _позиция));
                 }
             }
-            else if (CurrentChar() == '+' || CurrentChar() == '-' || CurrentChar() == '/' || CurrentChar() == '*')
+            else if (ТекущийСимвол() == '+' || ТекущийСимвол() == '-' || ТекущийСимвол() == '/' || ТекущийСимвол() == '*')
             {
-                var symbol = ReadWhile(c => c == '+' || c == '-' || c == '/' || c == '*');
-                tokens.Add(new Lexem(symbol, LexemType.АрифметическийОператор, _position));
+                var символ = ReadWhile(c => c == '+' || c == '-' || c == '/' || c == '*');
+                var арифметическиеСравнения = SymbolTable.ТаблицаЗаданныхСимволов().GroupBy(i => i.Item3).FirstOrDefault(i => i.Key == ТипЛексемы.АрифметическийОператор).Select(i => i.Item1).ToList();
+                if (арифметическиеСравнения.Contains(символ))
+                {
+                    лексемы.Add(new Лексема(символ, ТипЛексемы.АрифметическийОператор, _позиция));
+                }
+                else
+                {
+                    лексемы.Add(new Лексема(символ, ТипЛексемы.Неизвестно, _позиция));
+                }
             }
             else
             {
-                tokens.Add(new Lexem(CurrentChar().ToString(), LexemType.Неизвестно, _position));
-                _position++;
+                лексемы.Add(new Лексема(ТекущийСимвол().ToString(), ТипЛексемы.Неизвестно, _позиция));
+                _позиция++;
             }
         }
 
-        return tokens;
+        return лексемы;
     }
 
-    private char CurrentChar()
+    private char ТекущийСимвол()
     {
-        return _input[_position];
+        return _входнаяСтрока[_позиция];
     }
 
     private string ReadWhile(Func<char, bool> condition)
     {
-        var start = _position;
-        while (_position < _input.Length && condition(_input[_position]))
+        var start = _позиция;
+        while (_позиция < _входнаяСтрока.Length && condition(_входнаяСтрока[_позиция]))
         {
-            _position++;
+            _позиция++;
         }
 
-        return _input.Substring(start, _position - start);
+        return _входнаяСтрока.Substring(start, _позиция - start);
     }
 
     private bool IsLetter(char ch) => char.IsLetter(ch);
 
     private bool IsLetterOrDigit(char ch) => char.IsLetterOrDigit(ch);
 
-    private LexemType IdentifyKeyword(string value)
+    private ТипЛексемы IdentifyKeyword(string value)
     {
         switch (value)
         {
-            case "while": return LexemType.КлючевоеСлово_While;
-            case "do": return LexemType.КлючевоеСлово_Do;
-            case "loop": return LexemType.КлючевоеСлово_End;
-            case "and": return LexemType.КлючевоеСлово_And;
-            case "or": return LexemType.КлючевоеСлово_Or;
-            default: return LexemType.Идентификатор;
+            case "while": return ТипЛексемы.КлючевоеСлово_While;
+            case "do": return ТипЛексемы.КлючевоеСлово_Do;
+            case "loop": return ТипЛексемы.КлючевоеСлово_End;
+            case "and": return ТипЛексемы.КлючевоеСлово_And;
+            case "or": return ТипЛексемы.КлючевоеСлово_Or;
+            default: return ТипЛексемы.Идентификатор;
         }
     }
 }
-public enum LexemType
+public enum ТипЛексемы
 {
     КлючевоеСлово_While,
     КлючевоеСлово_Do,
@@ -173,7 +175,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        TerminalSymbolTable.PrintTable();
+        SymbolTable.ВывестиТаблицу();
         Console.WriteLine();
 
         while (true)
@@ -182,34 +184,33 @@ public class Program
             string input = Console.ReadLine();
             Console.WriteLine();
 
-            if (input.ToLower() == "exit")
+            if (input == "exit")
             {
                 break;
             }
-            else if (input.ToLower() == "menu")
+            else if (input == "ter")
             {
-                TerminalSymbolTable.PrintTable();
+                SymbolTable.ВывестиТаблицу();
             }
             else
             {
-                var lexer = new Lexer(input);
-                var tokens = lexer.Analyze();
+                var лексемы = new ЛексическийАнализ(input).Анализ();
 
-                foreach (var token in tokens)
+                foreach (var лексема in лексемы)
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    if (token.Type == LexemType.Неизвестно)
+                    if (лексема.Type == ТипЛексемы.Неизвестно)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                     }
-                    Console.Write($"{token.Type} : ");
+                    Console.Write($"{лексема.Type} : ");
                     Console.ResetColor();
                     Console.BackgroundColor = ConsoleColor.DarkBlue;
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write($"{ token.Value}");
+                    Console.Write($"{лексема.Value}");
                     Console.ResetColor();
                     Console.Write($" столбец ");
-                    Console.WriteLine($"{ token.Position}");
+                    Console.WriteLine($"{лексема.Position}");
                 }
             }
             Console.WriteLine();
